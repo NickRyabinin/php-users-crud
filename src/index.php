544632746088ba8965804d88c1bc296103f8dfbb
Николай <rnn2002@mail.ru>
2024-09-02 17:56:50 +0300

    Add index.php - single entry point to the application

---
+++ src/index.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Единая точка входа в приложение.
+ * Запускает подключение к БД, миграцию (только создание таблиц).
+ */
+
+/**
+ * Composer для автозагрузки классов не применяется - используется
+ * spl_autoload_register()
+ */
+
+spl_autoload_register(function ($className) {
+    $file = __DIR__ . '/../' . str_replace('\\', '/', $className) . '.php';
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
+
+use src\Database;
+
+const MIGRATION_PATH = __DIR__ . '/migration.sql';
+
+$pdo = Database::get()->connect();
+Database::get()->migrate($pdo, MIGRATION_PATH);
