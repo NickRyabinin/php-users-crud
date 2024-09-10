3f5ca9c7f9b727b0f5aa9c638f86ed91b109b218
Николай <rnn2002@mail.ru>
2024-09-10 18:03:52 +0300

    Modify View - add render() method

--- app/src/View.php
+++ app/src/View.php
@@ -4,4 +4,11 @@ namespace src;
 
 class View
 {
+    public function render(string $template): void
+    {
+        header('Content-Type: text/html');
+        header('Access-Control-Allow-Origin: *');
+        http_response_code('200');
+        echo file_get_contents(__DIR__ . '/../templates/' . $template . 'html');
+    }
 }
