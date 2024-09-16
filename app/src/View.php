9ea8095c4f99c8b72a48bf4643c225b5642c3215
Николай <rnn2002@mail.ru>
2024-09-16 18:32:40 +0300

    Modify View - rewright render() method to show templates correctly

--- app/src/View.php
+++ app/src/View.php
@@ -4,11 +4,31 @@ namespace src;
 
 class View
 {
-    public function render(string $template): void
+    private $templatesPath;
+
+    public function __construct($templatesPath)
     {
-        header('Content-Type: text/html');
+        $this->templatesPath = $templatesPath;
+    }
+
+    public function render(string $templateName, array $params): void
+    {
+        // Устанавливаем заголовки
+        header('Content-Type: text/html; charset=UTF-8');
         header('Access-Control-Allow-Origin: *');
-        http_response_code('200');
-        echo file_get_contents(__DIR__ . '/../templates/' . $template . 'html');
+
+        // Извлекаем параметры в переменные!
+        extract($params);
+
+        // Подключаем шаблон
+        $template = $this->templatesPath . $templateName . '.phtml';
+        
+        if (file_exists($template)) {
+            http_response_code(200);
+            include $template;
+        } else {
+            http_response_code(500);
+            echo "Шаблон не найден: " . htmlspecialchars($template);
+        }
     }
 }
