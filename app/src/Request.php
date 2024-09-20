2ba6cac61e0121c0d0093708a560f98f3e823c72
Николай <rnn2002@mail.ru>
2024-09-20 18:04:46 +0300

    Modify Request - simplify getId() method because nested resources are not planned

--- app/src/Request.php
+++ app/src/Request.php
@@ -8,34 +8,32 @@ namespace src;
 
 class Request
 {
+    public function getRequest(): array
+    {
+        return explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+    }
+
     public function getHttpMethod(): string
     {
         return strtoupper($_SERVER['REQUEST_METHOD']);
     }
 
-    public function getId(string $nested = ''): string | bool
+    public function getId(): string | bool
     {
         $request = $this->getRequest();
-        if ($nested) {
-            $id = isset($_GET['page']) ? '' : $request[3] ?? '';
-        } else {
-            $id = isset($_GET['page']) && !isset($request[3]) ? '' : $request[1] ?? '';
+        $id = $request[1] ?? '';
+
+        if ($id !== '' && preg_match('/^\d+$/', $id)) {
+            return (string)$id;
         }
-        if ($id !== '') {
-            return (preg_match('/^\d+$/', $id)) ? (string)$id : false;
-        }
-        return '';
+
+        return false;
     }
 
     public function getPage(): string
     {
         $page = isset($_GET['page']) ? $_GET['page'] : 1;
         return (preg_match('/^[1-9]\d*$/', $page)) ? (string)$page : 1;
-    }
-
-    public function getRequest(): array
-    {
-        return explode('/', trim($_SERVER['REQUEST_URI'], '/'));
     }
 
     public function getResource(string $parent = ''): string
@@ -51,17 +49,5 @@ class Request
     public function getInputData(): array
     {
         return json_decode(file_get_contents('php://input'), true) ?? [];
-    }
-
-    public function getToken(): string
-    {
-        $headers = getallheaders();
-        if (isset($headers['Authorization'])) {
-            $authHeader = $headers['Authorization'];
-            $bearerToken = explode(' ', $authHeader);
-            $token = $bearerToken[1];
-            return $token;
-        }
-        return '';
     }
 }
