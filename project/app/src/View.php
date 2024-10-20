e99876491e46bbd6f31facdaeb70cdb78610e090
Николай <rnn2002@mail.ru>
2024-10-20 15:06:16 +0300

    Modify View.php draft - add more arguments to render() method

--- project/app/src/View.php
+++ project/app/src/View.php
@@ -11,7 +11,7 @@ class View
         $this->templatesPath = $templatesPath;
     }
 
-    public function render(string $templateName, array $params): void
+    public function render(string $templateName, array $params = [], string $title = '', string $menu = ''): void
     {
         // Устанавливаем заголовки
         header('Content-Type: text/html; charset=UTF-8');
@@ -20,15 +20,24 @@ class View
         // Извлекаем параметры в переменные!
         extract($params);
 
-        // Подключаем шаблон
+        // Подключаем шаблоны в макет
         $template = $this->templatesPath . $templateName . '.phtml';
         
+        // тут нужно разобраться с шаблоном $menu
+
         if (file_exists($template)) {
             http_response_code(200);
-            include $template;
+            include $this->templatesPath . 'layout.phtml';
         } else {
             http_response_code(500);
             echo "Шаблон не найден: " . htmlspecialchars($template);
         }
     }
 }
+
+/*
+$params = [
+    'user' => $user
+];
+$view->render('edit', $params, 'Изменение пользователя', $menu);
+*/
