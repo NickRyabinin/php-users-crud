de44c705b1dade180ac253de2df23b1c49743327
Николай <rnn2002@mail.ru>
2024-10-25 18:13:58 +0300

    Modify View - remove $menu parameter from render() method

--- project/app/src/View.php
+++ project/app/src/View.php
@@ -11,37 +11,29 @@ class View
         $this->templatesPath = $templatesPath;
     }
 
-    public function render(string $templateName, array $params = [], string $title = '', string $menu = ''): void
+    public function render(string $templateName, array $data = [], string $title = ''): void
     {
         // Устанавливаем заголовки
         header('Content-Type: text/html; charset=UTF-8');
         header('Access-Control-Allow-Origin: *');
 
         // Извлекаем параметры в переменные!
-        extract($params);
+        extract($data);
 
         // Подключаем шаблоны в макет
-        $template = $this->templatesPath . $templateName . '.phtml';
-        // тут нужно разобраться с шаблоном $menu
+        $contentTemplate = $this->templatesPath . $templateName . '.phtml';
 
-        if (file_exists($template)) {
+        if (file_exists($contentTemplate)) {
             http_response_code(200);
             ob_start();
-            include $template;
+            include $contentTemplate;
             $content = ob_get_clean(); // Записываем содержимое буфера в $content для вставки в макет
         } else {
             http_response_code(500);
-            echo "Шаблон не найден: " . htmlspecialchars($template);
+            echo "Шаблон не найден: " . htmlspecialchars($contentTemplate);
             return;
         }
         // Выводим макет
         include $this->templatesPath . 'layout.phtml';
     }
 }
-
-/*
-$params = [
-    'user' => $user
-];
-$view->render('edit', $params, 'Изменение пользователя', $menu);
-*/
