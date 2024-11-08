b4d010377ee40a6947f39bc55fe008713abf0e64
Николай <rnn2002@mail.ru>
2024-11-08 14:22:50 +0300

    Modify UserController - make next iteration of update() method

--- project/app/src/UserController.php
+++ project/app/src/UserController.php
@@ -28,24 +28,6 @@ class UserController
     public function create(): void
     {
         $this->view->render('users/create', [], 'Создание пользователя');
-        /*
-        $inputData = $this->request->getInputData();
-        $cleanData = array_map(fn ($param) => $this->request->sanitize($this->request->validate($param)), $inputData);
-        $login = $cleanData['login'] ?? '';
-        $email = filter_var($cleanData['email'] ?? '', FILTER_VALIDATE_EMAIL);
-        if (!($login && $email)) {
-            parent::handleInvalidData();
-            return;
-        }
-        $hashedPassword = hash('sha256', $password);
-        $cleanData['hashed_password'] = $hashedPassword;
-        try {
-            $this->user->store($cleanData);
-            parent::handleUserCreatedOk();
-        } catch (InvalidDataException $e) {
-            parent::handleInvalidData();
-        }
-        */
     }
 
     public function store()
@@ -69,7 +51,7 @@ class UserController
         if ($uploadedFile && $uploadedFile['error'] === UPLOAD_ERR_OK) {
             $uploadDir = __DIR__ . '/../assets/avatars/';
             $profilePicture = $uploadDir . basename($uploadedFile['name']);
-        
+
             // Перемещаем загруженный файл в нужную директорию
             if (move_uploaded_file($uploadedFile['tmp_name'], $profilePicture)) {
                 // Файл ОК
@@ -127,22 +109,48 @@ class UserController
 
     public function update(): void
     {
-        echo "UserController->update() invoked";
-        /*
-        $id = $this->request->getId();
-        $inputData = $this->request->getInputData();
-        $cleanData = array_map(fn ($param) => $this->request->sanitize($this->request->validate($param)), $inputData);
-        try {
-            $this->user->update($id, $cleanData);
-            parent::handleUpdatedOk();
-        } catch (InvalidIdException $e) {
-            parent::handleNoRecord();
-        } catch (InvalidTokenException $e) {
-            parent::handleInvalidToken();
-        } catch (InvalidDataException $e) {
-            parent::handleInvalidData();
+        $id = $this->request->getResourceId();
+
+        $login = $this->request->getFormData('username');
+        $email = $this->request->getFormData('email');
+        $password = $this->request->getFormData('password');
+        $confirmPassword = $this->request->getFormData('confirm_password');
+        $role = $this->request->getFormData('role');
+        $isActive = $this->request->getFormData('is_active') ? 'true' : 'false';
+
+        $uploadedFile = $this->request->getFile('profile_picture');
+        $profilePicture = '';
+
+        // Тут надо добавить валидацию данных и вывод флэша об ошибках
+        // разобраться с паролем
+        // проверить размер загруженного аватара
+
+        if ($uploadedFile && $uploadedFile['error'] === UPLOAD_ERR_OK) {
+            $uploadDir = __DIR__ . '/../assets/avatars/';
+            $profilePicture = $uploadDir . basename($uploadedFile['name']);
+
+            // Перемещаем загруженный файл в нужную директорию
+            if (move_uploaded_file($uploadedFile['tmp_name'], $profilePicture)) {
+                // Файл ОК
+            } else {
+                // Ошибка при загрузке файла
+            }
         }
-        */
+
+        $hashedPassword = hash('sha256', $password);
+        $this->user->update($id, [
+            'login' => $login,
+            'email' => $email,
+            'hashed_password' => $hashedPassword,
+            'profile_picture' => $profilePicture,
+            'is_active' => $isActive,
+            'role' => $role,
+        ]);
+        // Сюда флэш ОК
+
+        // Редирект на маршрут  GET /users
+        header('Location: /users');
+        exit();
     }
 
     public function delete(): void
@@ -157,16 +165,5 @@ class UserController
         // Сюда флэш ОК
         header('Location: /users');
         exit();
-
-        /*
-        try {
-            $this->user->destroy($id);
-            parent::handleDeletedOk();
-        } catch (InvalidTokenException $e) {
-            parent::handleInvalidToken();
-        } catch (InvalidDataException $e) {
-            parent::handleInvalidData();
-        }
-        */
     }
 }
