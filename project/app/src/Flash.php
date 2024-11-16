924cf38bd90052351c6e9ceb6d2d39ffc84a5f18
Николай <rnn2002@mail.ru>
2024-11-16 18:14:17 +0300

    Modify Flash.php - fix bug with multiple messages in set() method

--- project/app/src/Flash.php
+++ project/app/src/Flash.php
@@ -6,16 +6,21 @@ class Flash
 {
     public function set(string $key, string $message): void
     {
-        $_SESSION['flash'][$key] = $message;
+        // ! Инициализируем массив, если он не существует
+        if (!isset($_SESSION['flash'][$key])) {
+            $_SESSION['flash'][$key] = [];
+        }
+
+        $_SESSION['flash'][$key][] = $message;
     }
 
-    public function get(): ?array
+    public function get(): array
     {
         if (isset($_SESSION['flash'])) {
             $flash = $_SESSION['flash'];
             unset($_SESSION['flash']);
             return $flash;
         }
-        return null;
+        return [];
     }
 }
