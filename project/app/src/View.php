6f26ba67147079dd94fb2e71d762999fac4acf8a
Николай <rnn2002@mail.ru>
2024-12-12 16:11:48 +0300

    Modify View.php - move $title parameter into $data array parameter

--- project/app/src/View.php
+++ project/app/src/View.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Класс View выводит пользователю приложения указанные контроллером шаблоны страниц,
+ * включенные в макет и заполненные переданными данными.
+ */
+
 namespace src;
 
 class View
@@ -11,7 +16,7 @@ class View
         $this->templatesPath = $templatesPath;
     }
 
-    public function render(string $templateName, array $data = [], string $title = ''): void
+    public function render(string $templateName, array $data = []): void
     {
         // Устанавливаем заголовки
         header('Content-Type: text/html; charset=UTF-8');
@@ -30,7 +35,7 @@ class View
             $content = ob_get_clean(); // Записываем содержимое буфера в $content для вставки в макет
         } else {
             http_response_code(500);
-            echo "Шаблон не найден: " . htmlspecialchars($contentTemplate);
+            echo "Не найден шаблон для отображения";
             return;
         }
         // Выводим макет
