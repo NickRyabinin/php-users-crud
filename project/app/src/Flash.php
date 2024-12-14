9c97a5fd306bf7fcefa5ad1ddb40bdb2fbe418ec
Николай <rnn2002@mail.ru>
2024-12-14 14:43:43 +0300

    Modify Flash.php - change get() method - allow to get only named flash message

--- project/app/src/Flash.php
+++ project/app/src/Flash.php
@@ -1,12 +1,17 @@
 <?php
 
+/**
+ * Класс Flash предоставляет методы для сохранения и извлечения данных
+ * о промежуточном состоянии приложения с помощью суперглобального
+ * массива $_SESSION.
+ */
+
 namespace src;
 
 class Flash
 {
     public function set(string $key, string $message): void
     {
-        // ! Инициализируем массив, если он не существует
         if (!isset($_SESSION['flash'][$key])) {
             $_SESSION['flash'][$key] = [];
         }
@@ -14,13 +19,20 @@ class Flash
         $_SESSION['flash'][$key][] = $message;
     }
 
-    public function get(): array
+    public function get(string $key = ""): array
     {
+        $flash = [];
         if (isset($_SESSION['flash'])) {
-            $flash = $_SESSION['flash'];
-            unset($_SESSION['flash']);
-            return $flash;
+            if ($key) {
+                if (isset($_SESSION['flash'][$key])) {
+                    $flash = $_SESSION['flash'][$key];
+                    unset($_SESSION['flash'][$key]);
+                }
+            } else {
+                $flash = $_SESSION['flash'];
+                unset($_SESSION['flash']);
+            }
         }
-        return [];
+        return $flash;
     }
 }
