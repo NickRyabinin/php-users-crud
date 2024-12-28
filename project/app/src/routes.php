c33b1177a0a96373ef1eb8d5a0a032579f6a5c01
Николай <rnn2002@mail.ru>
2024-12-28 18:11:31 +0300

    Modify routes.php - add metadata to each route

--- project/app/src/routes.php
+++ project/app/src/routes.php
@@ -5,25 +5,25 @@ use src\PageController;
 
 return [
     'GET' => [
-        '/users/register' => [UserController::class, 'showRegistrationForm'],
-        '/users/captcha' => [UserController::class, 'showCaptcha'],
-        '/users/login' => [UserController::class, 'showLoginForm'],
-        '/users/logout' => [UserController::class, 'logout'],
-        '/users/new' => [UserController::class, 'create'],
-        '/users/{id}/edit' => [UserController::class, 'edit'],
-        '/users/{id}' => [UserController::class, 'show'],
-        '/users' => [UserController::class, 'index'],
-        '/' => [PageController::class, 'read']
+        '/users/register' => [UserController::class, 'showRegistrationForm', []],
+        '/users/captcha' => [UserController::class, 'showCaptcha', []],
+        '/users/login' => [UserController::class, 'showLoginForm', []],
+        '/users/logout' => [UserController::class, 'logout', ['auth' => true]],
+        '/users/new' => [UserController::class, 'create', ['admin' => true]],
+        '/users/{id}/edit' => [UserController::class, 'edit', ['auth' => true]],
+        '/users/{id}' => [UserController::class, 'show', ['auth' => true]],
+        '/users' => [UserController::class, 'index', ['admin' => true]],
+        '/' => [PageController::class, 'read', []]
     ],
     'POST' => [
-        '/users/register' => [UserController::class, 'register'],
-        '/users/login' => [UserController::class, 'login'],
-        '/users' => [UserController::class, 'store']
+        '/users/register' => [UserController::class, 'register', []],
+        '/users/login' => [UserController::class, 'login', []],
+        '/users' => [UserController::class, 'store', ['admin' => true]],
     ],
     'PUT' => [
-        '/users/{id}' => [UserController::class, 'update']
+        '/users/{id}' => [UserController::class, 'update', ['auth' => true]],
     ],
     'DELETE' => [
-        '/users/{id}' => [UserController::class, 'delete']
+        '/users/{id}' => [UserController::class, 'delete', ['admin' => true]],
     ],
 ];
