51b4b4043e230937b10fe1e8fff54316b8658359
Николай <rnn2002@mail.ru>
2024-12-29 16:57:53 +0300

    Modify AuthMiddleware - change methods by using injected Response

--- project/app/src/AuthMiddleware.php
+++ project/app/src/AuthMiddleware.php
@@ -5,27 +5,37 @@ namespace src;
 class AuthMiddleware
 {
     private Auth $auth;
+    private Response $response;
 
-    public function __construct(Auth $auth)
+    public function __construct(Auth $auth, Response $response)
     {
         $this->auth = $auth;
+        $this->response = $response;
     }
 
     public function checkAuth(): void
     {
         if (!$this->auth->isAuth()) {
-            header('Location: /users/login');
-            http_response_code(401);
-            exit();
+            $this->response->redirect(
+                '/users/login',
+                [
+                    'error' => 'Действие доступно только аутентифицированным пользователям',
+                    'status_code' => '401',
+                ]
+            );
         }
     }
 
     public function checkAdmin(): void
     {
         if (!$this->auth->isAdmin()) {
-            header('Location: /');
-            http_response_code(403);
-            exit();
+            $this->response->redirect(
+                '/',
+                [
+                    'error' => 'Действие доступно только пользователям с правами администратора',
+                    'status_code' => '403',
+                ]
+            );
         }
     }
 }
