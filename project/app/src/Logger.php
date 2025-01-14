514248ba391cfcb3bdc741e3ac84c97cc2ea4fbf
Николай <rnn2002@mail.ru>
2025-01-14 13:06:30 +0300

    Add new Logger class

---
+++ project/app/src/Logger.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Класс Logger - записывает передаваемые извне сообщения в указанный файл.
+ */
+
+namespace src;
+
+class Logger
+{
+    private string $logFile;
+
+    public function __construct(string $logFile)
+    {
+        $this->logFile = $logFile;
+    }
+
+    public function log(string $message): void
+    {
+        error_log($message . PHP_EOL, 3, $this->logFile);
+    }
+}
